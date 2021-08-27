fb16fe8c692a82ff81fbba4d9fcda254be8a0dd4
dizelvinable <sadr3479@gmail.com>
2021-08-28 00:20:48 +0300

    166. Level 3 - Gold Medal - Some tmp changes

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -33,21 +33,25 @@ const List<String> cryptoList = [
 
 class CoinData {
   Future getCoinData({currency = 'USD'}) async {
-    final response = await http.get(
-      Uri.parse('https://rest.coinapi.io/v1/exchangerate/BTC/$currency'),
-      headers: <String, String>{
-        'X-CoinAPI-Key': '729831F7-9526-406C-A265-595C900079F6',
-      },
+    // final response = await http.get(
+    //   Uri.parse('https://rest.coinapi.io/v1/exchangerate/BTC/$currency'),
+    //   headers: <String, String>{
+    //     'X-CoinAPI-Key': '729831F7-9526-406C-A265-595C900079F6',
+    //   },
+    // );
+    //
+    // if (response.statusCode == 200) {
+    //   var decodedData = jsonDecode(response.body);
+    //   var lastPrice = decodedData['rate'];
+    //   print(lastPrice);
+    //   return lastPrice;
+    // } else {
+    //   print(response.statusCode);
+    //   throw Exception('Problem with the get request');
+    // }
+    Future.delayed(
+      const Duration(seconds: 2),
+      () => 1.1,
     );
-
-    if (response.statusCode == 200) {
-      var decodedData = jsonDecode(response.body);
-      var lastPrice = decodedData['rate'];
-      print(lastPrice);
-      return lastPrice;
-    } else {
-      print(response.statusCode);
-      throw Exception('Problem with the get request');
-    }
   }
 }
